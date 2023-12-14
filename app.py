9a24ed79abc8cdacf89a69ed9e289967044b673e
Omaima <omaimasharhan@gmail.com>
2023-12-13 20:46:02 -0500

    update

--- app.py
+++ app.py
@@ -1,47 +1,77 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = 'my_secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-
 db = SQLAlchemy(app)
 
-# Define your models (e.g., User, UserBook)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
 
+with app.app_context():
+    user = User.query.filter_by(username='adam').first()
+    if not user:
+        hashed_password = generate_password_hash('password123')
+        new_user = User(username='adam', password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+
 class UserBook(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(200))
-    author = db.Column(db.String(200))
+    title = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(200), nullable=False)
     page_count = db.Column(db.Integer)
-    average_rating = db.Column(db.Float)
-    user_id = db.Column(db.String(100))
+    average_rating = db.Column(db.REAL)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+    def __repr__(self):
+        return f'<UserBook {self.title}>'
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    error = None
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-
         user = User.query.filter_by(username=username).first()
 
-        if user and password == user.password:
+        if user and check_password_hash(user.password, password):
+            if username != 'adam':
+                error = "You can only log in as 'adam'"
+                flash(error, 'error')
+                return render_template('login.html', error=error)
+
             session['logged_in'] = True
             session['username'] = username
-            return redirect(url_for('user_books'))
+            flash('Login successful', 'success')
+            return redirect(url_for('dashboard'))
         else:
-            return render_template('login.html', error='Invalid credentials')
+            error = 'Invalid username or password'
+            flash(error, 'error')
 
-    return render_template('login.html')
+    return render_template('login.html', error=error)
+
+
+@app.route('/dashboard')
+def dashboard():
+    if 'logged_in' in session:
+        print("Rendering dashboard")
+        return render_template('dashboard.html')
+    else:
+        print("Redirecting to login")
+        return redirect(url_for('login'))
 
 @app.route('/')
 def index():
-    if 'logged_in' in session:
-        return redirect(url_for('user_books'))
     return redirect(url_for('login'))
+
 
 @app.route('/logout')
 def logout():
@@ -71,16 +101,23 @@ def user_books():
                 average_rating=book_data['average_rating'],
                 user_id=session['username']  # Assuming user_id here is the username
             )
-            db.session.add(new_book)
-            db.session.commit()
+            try:
+                db.session.add(new_book)
+                db.session.commit()
+                print("Book added to the database")
+            except Exception as e:
+                print("Error adding book:", e)
 
             return redirect(url_for('user_books'))
 
         else:
             user_books = UserBook.query.filter_by(user_id=session['username']).all()
+            print("Books retrieved from the database:", user_books)
             return render_template('user_books.html', books=user_books)
     else:
         return redirect(url_for('login'))
+
+
 
 
 with app.app_context():
---
+++ user_books.sql
@@ -0,0 +1,16 @@
+CREATE TABLE user_books (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id TEXT NOT NULL,
+    title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    page_count INTEGER,
+    average_rating REAL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL
+);
